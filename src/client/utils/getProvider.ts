cdccf191e0c32f1063173997474ae749b2d6a27d
Chef Snoopy <snoopy@pancakeswap.com>
2025-03-24 14:20:43 +0800

    feat: Update provider using env

--- src/client/utils/getProvider.ts
+++ src/client/utils/getProvider.ts
@@ -2,19 +2,21 @@ import * as anchor from '@project-serum/anchor';
 import { Program, AnchorProvider, web3 } from '@project-serum/anchor';
 import { Connection, PublicKey, Keypair, clusterApiUrl } from '@solana/web3.js';
 import bs58 from 'bs58';
-// import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
-// dotenv.config();
+dotenv.config();
 
-let keypair: Keypair = Keypair.generate();
+let keypair: Keypair;
+if (process.env.PRIVATE_KEY) {
+  const secretKey = bs58.decode(process.env.PRIVATE_KEY);
+  keypair = Keypair.fromSecretKey(secretKey);
+} else {
+  keypair = Keypair.generate();
+}
 
-// const network = (process.env.NETWORK as web3.Cluster) || "devnet";
-const network = 'devnet';
-
-// 配置 Solana Devnet 连接
+const network = (process.env.NETWORK as web3.Cluster) || 'devnet';
 const connection = new Connection(clusterApiUrl(network), 'confirmed');
 
-// 设置 Anchor Provider
 const provider = new AnchorProvider(connection, new anchor.Wallet(keypair), {
   preflightCommitment: 'confirmed',
 });
